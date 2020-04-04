a4330dd27958c135f354b8141f54ce867fa1e3ed
gavasheli <you@example.com>
2020-04-04 17:32:19 +0400

    add css

---
+++ davaleba.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"/>
+<link rel="stylesheet" href="/css/"/>
+<title>Page Title</title>
+<style>
+body{
+    background-color: white;
+    text-align:center;
+    display: block;
+}
+.menu-list-item{
+    display:inline-block;
+    margin:0 15px;
+    line-height: 60px;
+}
+.menu{
+    z-index:1000;
+    float: right;
+    font-size:15px;
+    position:fixed;
+    top:0;
+    right:0;
+    transform: translateX(-120px);
+
+}
+</style>
+</head>
+<body>
+<nav class="menu">
+        <ul class="menu-list">
+          <li class="menu-list-item">
+             <a href="#link">about</a></li>
+               <li class="menu-list-item">
+                  <a href="#link">cooperation</a></li>
+                        <li class="menu-list-item">
+                  <a href="#link">contact</a></li>
+               
+
+
+</body>
+</html>
