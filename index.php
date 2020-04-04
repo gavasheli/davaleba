6f751ecff38c22ce66a76a1b98b1ab572aae5631
gavasheli <you@example.com>
2020-04-04 17:06:16 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+<nav class="menu">
+        <ul class="menu-list">
+          <li class="menu-list-item">
+             <a href="#link">HOME</a></li>
+               <li class="menu-list-item">
+                  <a href="#link">SERVICE</a></li>
+                        <li class="menu-list-item">
+                  <a href="#link">ABOUT US</a></li>
+                <li class="menu-list-item">
+               <a href="#link">CONTACT</a></li>
+            <li class="menu-list-item">
+          <a href="#link">FAQ</a></li> 
+       <li class="menu-list-item">
+     <a href="#link">OUR CUSTOMERS</a></li>
+
+
+</body>
+</html>
